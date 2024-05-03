e37dd124edec76da951462a4992e50d042f2eecb
di-cherta <di.cherta@gmail.com>
2024-05-03 23:15:07 +0300

    add changes for list

--- src/Components/Editing/Editing.jsx
+++ src/Components/Editing/Editing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import image5 from '../assets/check-button.png';
+import image6 from '../assets/cancel-button.png';
 import Styles from './Editing.module.css';
 
 export default function Editing({word, transcription, translation}) {
@@ -8,10 +9,10 @@ export default function Editing({word, transcription, translation}) {
       return(
       <React.Fragment>
         <tr>
-        <td><input value={word} /></td>
-        <td><input value={transcription} /></td>
-        <td><input value={translation} /></td>
-        <td style={{width: '120px'}}><button className={Button} id="check"><img src={image5} alt="check" className={Image}/></button></td>
+        <td><input placeholder={word} /></td>
+        <td><input placeholder={transcription} /></td>
+        <td><input placeholder={translation} /></td>
+        <td style={{width: '120px'}}><button className={Button} id="check"><img src={image5} alt="check" className={Image}/></button><button className={Button} id="cancel"><img src={image6} alt="check" className={Image}/></button></td>
       </tr>
       </React.Fragment>
       )
--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Styles from './List.module.css'
-import Table from '../Table/Table';
-import Editing from '../Editing/Editing';
+import { useState } from 'react';
+import image1 from '../assets/edit-button.png';
+import image2 from '../assets/delete-button.png';
+import image5 from '../assets/check-button.png';
+import image6 from '../assets/cancel-button.png';
+
 
 export default function List() {
   const List = (Styles.list);
   const Wrapper = (Styles.wrapper);
   const animals = [
-    {id: 1, word: 'cat', transcription: '[kæt]',translation: 'кошка'},
-    {id: 2, word: 'dog', transcription: '[dɒɡ]',translation: 'собака'},
-    {id: 3, word: 'hamster', transcription: '[ˈhæm.stər]',translation: 'хомяк'},
-    {id: 4, word: 'snake', transcription: '[sneɪk]',translation: 'змея'},
+    {id: 1, word: 'cat', transcription: '[kæt]',translation: 'кошка', finished: false },
+    {id: 2, word: 'dog', transcription: '[dɒɡ]',translation: 'собака', finished: false },
+    {id: 3, word: 'hamster', transcription: '[ˈhæm.stər]',translation: 'хомяк', finished: false },
+    {id: 4, word: 'snake', transcription: '[sneɪk]',translation: 'змея', finished: false },
   ];
-    const Changes = false;
+  const [list, setList] = useState(animals);
+  const [updatedItem, setUpdatedItem] = useState(null);
   return (
     <div className={Wrapper}>
         <Header/>
@@ -31,17 +36,88 @@ export default function List() {
           </tr>
         </thead>
         <tbody>
-          {animals.map((item)=>{
-            return(
-              <React.Fragment>
-            {Changes ?
-            <Editing key={item.id} word={item.word} transcription={item.transcription} translation={item.translation} changes={item.changes}/>
-          : <Table key={item.id} word={item.word} transcription={item.transcription} translation={item.translation} changes={item.changes}/>}
-          </React.Fragment>
-        )})}
+          {list.map((item, index)=> (
+            <ListItem
+            key={item.id}
+            item={item}
+            index={index}
+            setList={setList}
+            updatedItem={updatedItem}
+            setUpdatedItem={setUpdatedItem}
+          />
+          ))}
         </tbody>
       </table>
       <Footer/>
     </div>
   );
 }
+const ListItem = ({
+  item: { id, word, transcription, translation},
+  setList,
+  updatedItem,
+  setUpdatedItem,
+}) => {
+  const Image = (Styles.image);
+  const Button = (Styles.button);
+
+  const isCurrentBeingUpdated = updatedItem === id;
+
+  const handleInputChange = ({ target: { value } }) => {
+    setList((prevList) =>
+      prevList.map((item) =>
+        item.id === id ? { ...item, word: value} : item
+      )
+    );
+  };
+  const handleTranscriptionChange = ({ target: { value } }) => {
+    setList((prevList) =>
+      prevList.map((item) =>
+        item.id === id ? { ...item, transcription: value} : item
+      )
+    );
+  };
+  const handleTranslationChange = ({ target: { value } }) => {
+    setList((prevList) =>
+      prevList.map((item) =>
+        item.id === id ? { ...item, translation: value} : item
+      )
+    );
+  };
+
+  const renderWordOrInput = () => {
+    return isCurrentBeingUpdated ? (
+      <input value={word} onChange={handleInputChange} />
+    ) : (
+      word
+    );
+  };
+  const renderTranscriptionOrInput = () => {
+    return isCurrentBeingUpdated ? (
+      <input value={transcription} onChange={handleTranscriptionChange} />
+    ) : (
+      transcription
+    );
+  };
+  const renderTranslationOrInput = () => {
+    return isCurrentBeingUpdated ? (
+      <input value={translation} onChange={handleTranslationChange} />
+    ) : (
+      translation
+    );
+  };
+
+  return (
+    <tr>
+      <td>{renderWordOrInput()}</td>
+      <td>{renderTranscriptionOrInput()}</td>
+      <td>{renderTranslationOrInput()}</td>
+      <td style={{width: '120px'}}><button onClick={() => setUpdatedItem(isCurrentBeingUpdated ? null : id)} className={Button}>
+        {isCurrentBeingUpdated ? <img src={image5} alt="check" className={Image}/> : <img src={image1} alt="edit" className={Image}/>}
+      </button>
+      <button className={Button}><img src={image6} alt="check" className={Image}/></button>
+      </td>
+      </tr>
+  );
+};
+
